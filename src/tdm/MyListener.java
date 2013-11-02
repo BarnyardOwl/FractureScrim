235d9aaacf59f913e29c9d1976b9af4f84ff6ac7
Barnyard_Owl <Barnyard.Owl.1@gmail.com>
2013-11-02 18:13:29 -0400

    Added a few more event stubs.

--- src/tdm/MyListener.java
+++ src/tdm/MyListener.java
@@ -9,6 +9,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MyListener implements Listener {
 
@@ -26,9 +29,28 @@ public class MyListener implements Listener {
 	}
 
 	@EventHandler
+	public void onPlayerJoin( PlayerJoinEvent event ) {
+		// TODO: Set player's team to observer
+	}
+
+	@EventHandler
+	public void onPlayerLeave( PlayerQuitEvent event ) {
+		if( Var.teams.containsKey( event.getPlayer( ).getName( ) ) ) { // Be SURE to use getName() and not getDisplayName()!
+			Var.teams.remove( event.getPlayer( ).getName( ) );
+		}
+	}
+
+	@EventHandler
+	public void onPlayerKick( PlayerKickEvent event ) {
+		if( Var.teams.containsKey( event.getPlayer( ).getName( ) ) ) { // Be SURE to use getName() and not getDisplayName()!
+			Var.teams.remove( event.getPlayer( ).getName( ) );
+		}
+	}
+
+	@EventHandler
 	public void onPlayerDamaged( EntityDamageByEntityEvent event ) {
 		if( event.getDamager( ) instanceof Player && event.getEntity( ) instanceof Player ) {
-			// TODO Cancel event if damager is spectator.
+			// TODO: Cancel event if damager is spectator.
 		}
 	}
 
